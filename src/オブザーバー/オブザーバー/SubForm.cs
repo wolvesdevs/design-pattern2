88ae4ecc31c464336670032fa5ca13e40d337a46
wolvesdevs_workPC <75551104+wolvesdevs@users.noreply.github.com>
2023-11-17 09:26:59 +0900

    #02_Observerパターンを使わない場合の問題点

---
+++ src/オブザーバー/オブザーバー/SubForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace オブザーバー
+{
+    public partial class SubForm : Form
+    {
+        public SubForm()
+        {
+            InitializeComponent();
+
+            StartPosition = FormStartPosition.CenterScreen;
+
+            timer1.Interval = 5000;
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(WarningTimer.IsWarning)
+            {
+                WarningLabel.Text = "警報";
+                WarningLabel.BackColor = Color.Red;
+            }
+            else
+            {
+                WarningLabel.Text = "正常";
+                WarningLabel.BackColor = Color.Lime;
+            }
+        }
+    }
+}
