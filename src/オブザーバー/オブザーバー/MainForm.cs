4fd42e8b4f0320733fc32437b364abb351f85ea6
wolvesdevs_homePC <75551104+wolvesdevs@users.noreply.github.com>
2023-11-19 01:53:36 +0900

    refs #08_ViewとViewModelに分けてINotifyPropertyChangedを実装する

--- src/オブザーバー/オブザーバー/MainForm.cs
+++ src/オブザーバー/オブザーバー/MainForm.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Threading;
 
 namespace オブザーバー
 {
-    public partial class MainForm : Form, INotify
+    public partial class MainForm : Form
     {
+        private MainViewModel _vm = new MainViewModel(Dispatcher.CurrentDispatcher);
+
         public MainForm()
         {
             InitializeComponent();
@@ -19,30 +22,13 @@ namespace オブザーバー
             this.Disposed += MainForm_Disposed;
             StartPosition = FormStartPosition.CenterScreen;
             //WarningTimer.WarningAction += WarningTimer_WarningAction;
-            WarningTimer.Add(this);
-        }
 
-        public void Update(bool isWarning)
-        {
-            this.Invoke((Action)delegate ()
-            {
-                if (isWarning)
-                {
-                    WarningLabel.Text = "警報";
-                    WarningLabel.BackColor = Color.Red;
-                }
-                else
-                {
-                    WarningLabel.Text = "正常";
-                    WarningLabel.BackColor = Color.Lime;
-                }
-            });
+            WarningLabel.DataBindings.Add("Text", _vm, nameof(_vm.WarningLabelText));
         }
 
         private void MainForm_Disposed(object sender, EventArgs e)
         {
-            //WarningTimer.WarningAction -= WarningTimer_WarningAction;
-            WarningTimer.Remove(this);
+            _vm?.Dispose();
         }
 
         private void SubButton_Click(object sender, EventArgs e)
---
+++ src/オブザーバー/オブザーバー/MainViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace オブザーバー
+{
+    public class MainViewModel : INotifyPropertyChanged, INotify, IDisposable
+    {
+        Dispatcher _dispatcher;
+
+        public MainViewModel(Dispatcher dispatcher)
+        {
+            _dispatcher = dispatcher;
+            WarningTimer.Add(this);
+        }
+
+        public void Dispose()
+        {
+            WarningTimer.Remove(this);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _warningLabelText = "AAA";
+
+        public string WarningLabelText
+        {
+            get { return _warningLabelText; }
+            set
+            {
+                if (_warningLabelText != value)
+                {
+                    _warningLabelText = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(WarningLabelText)));
+                }
+            }
+        }
+
+        public void Update(bool isWarning)
+        {
+            _dispatcher.Invoke((Action)delegate ()
+            {
+                if (isWarning)
+                {
+                    WarningLabelText = "警報";
+                    //WarningLabel.BackColor = Color.Red;
+                }
+                else
+                {
+                    WarningLabelText = "正常";
+                    //WarningLabel.BackColor = Color.Lime;
+                }
+            });
+        }
+    }
+}
